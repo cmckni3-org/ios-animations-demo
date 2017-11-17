f27fdde7341c7d9caa109a17ad12d88217508f80
Chris McKnight <cmckni3@gmail.com>
2017-11-17 00:10:00 -0600

    Remove commented out code

--- Animations/ViewController.m
+++ Animations/ViewController.m
@@ -28,35 +28,6 @@
 {
     CALayer *theLayer = self.label.layer;
     
-//    // Animation 1
-//    CAKeyframeAnimation* widthAnim = [CAKeyframeAnimation animationWithKeyPath:@"borderWidth"];
-//    NSArray* widthValues = [NSArray arrayWithObjects:@1.0, @10.0, @5.0, @30.0, @0.5, @15.0, @2.0, @50.0, @0.0, nil];
-//    widthAnim.values = widthValues;
-//    widthAnim.calculationMode = kCAAnimationPaced;
-//    
-//    // Animation 2
-//    CAKeyframeAnimation* colorAnim = [CAKeyframeAnimation animationWithKeyPath:@"borderColor"];
-//    NSArray* colorValues = [NSArray arrayWithObjects:(id)[UIColor greenColor].CGColor,
-//                            (id)[UIColor redColor].CGColor, (id)[UIColor blueColor].CGColor,  nil];
-//    colorAnim.values = colorValues;
-//    colorAnim.calculationMode = kCAAnimationPaced;
-//    
-//    // Animation group
-//    CAAnimationGroup* group = [CAAnimationGroup animation];
-//    group.animations = [NSArray arrayWithObjects:colorAnim, widthAnim, nil];
-//    group.duration = 5.0;
-//    
-//    [theLayer addAnimation:group forKey:@"BorderChanges"];
-    
-//    CABasicAnimation* fadeAnim = [CABasicAnimation animationWithKeyPath:@"opacity"];
-//    fadeAnim.fromValue = [NSNumber numberWithFloat:1.0];
-//    fadeAnim.toValue = [NSNumber numberWithFloat:0.0];
-//    fadeAnim.duration = 1.0;
-//    [theLayer addAnimation:fadeAnim forKey:@"opacity"];
-    
-    // Change the actual data value in the layer to the final value.
-//    theLayer.opacity = 0.0;
-    
     [UIView animateWithDuration:1.0 animations:^{
         theLayer.backgroundColor = [UIColor blueColor].CGColor;
     } completion:^(BOOL finished){
